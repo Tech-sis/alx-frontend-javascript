d1747566f36808fc5eeb08743a0eee8ffbed2040
Tech-sis <abilawonadeola@gmail.com>
2022-12-22 17:58:01 -0500

    fix syntax error

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,7 @@
 export default class Building {
   constructor(sqft) {
     if (
-      new.target !== Building
-			&& typeof this.evacuationWarningMessage !== 'function'
+      new.target !== Building && typeof this.evacuationWarningMessage !== 'function'
     ) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
