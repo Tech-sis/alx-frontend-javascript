03b88622990d6ca6f92f3e858f80ac920c6862a1
Tech-sis <abilawonadeola@gmail.com>
2023-01-03 22:08:46 -0500

    fix errors

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
-const getStudentIdsSum = (arr) => {
-	return arr.reduce((accum, current) => accum + current.id, 0);
-}
+const getStudentIdsSum = (arr) => arr.reduce((accum, current) => accum + current.id, 0);
 
-export default getStudentIdsSum
+export default getStudentIdsSum;
--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,2 @@
-const updateStudentGradeByCity = (arr, city, newGrades) => {
-	return arr.filter((value) => value.location === city).map((value) => ({ ...value, grade: newGrades.filter((grad) => grad.studentId === value.id).map((grad) => grad.grade)[0] || 'N/A' }))
-}
-export default updateStudentGradeByCity
+const updateStudentGradeByCity = (arr, city, newGrades) => arr.filter((value) => value.location === city).map((value) => ({ ...value, grade: newGrades.filter((grad) => grad.studentId === value.id).map((grad) => grad.grade)[0] || 'N/A' }));
+export default updateStudentGradeByCity;
